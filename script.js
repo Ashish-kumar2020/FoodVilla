f79a78d67cccfbbea6e163a79cdf64b637bfab7b
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-01-11 07:57:57 +0530

    .

--- script.js
+++ script.js
@@ -41,11 +41,11 @@ import '/style.css';
     
     
 **/
-const Title = ()=>{
-    <a href="/">
-        <img className='logo' src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" alt="logo" />
-    </a>
-}
+// const Title = ()=>{
+//     <a href="/">
+//         <img className='logo' src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" alt="logo" />
+//     </a>
+// }
 const HeaderComp = ()=>{
     return (
         // <div><h1>React from Functional</h1></div>
